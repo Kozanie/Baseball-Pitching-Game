cb1786397fc200593c52b473ab79352bcee501c5
jeremiah.gerlach <jeremiah.gerlach@MU-C1-18.mullen.pvt.k12.co.us>
2023-11-08 10:49:42 -0700

    Start of game

--- Individual Project/src/PitcherGame.java
+++ Individual Project/src/PitcherGame.java
@@ -1,14 +1,29 @@
-
+import java.util.ArrayList;
+import java.util.Scanner;
 public class PitcherGame
 	{
-
+		
 		public static void main(String[] args)
 			{
-			//added during class 
-			System.out.println("Hello World");
+			startGame();
+			}
+		
+		public static void startGame()
+			{
+			String dummy;
+			Scanner userInput = new Scanner(System.in);
 			
-			//added @ home
-			System.out.println("Hello");
+			System.out.println("You will be playing a baseball pitching simulator! You will be chosing what pitches you want to throw!");
+			System.out.println("\nPress enter to continue!");
+			dummy = userInput.nextLine();
+			
+			System.out.println("Shohei Ohtani will be pitching against Mike Trout. \n\nPress enter to continue.");
+			dummy = userInput.nextLine();
+			}
+		
+		public static void playGame()
+			{
+			
 			}
 
 	}
