ba548d7d4e08c2e12d57e383b27aa4531a66888f
jgerl <jgerl@10.0.0.19>
2023-11-15 19:25:28 -0700

    game

--- Individual Project/src/PitcherGame.java
+++ Individual Project/src/PitcherGame.java
@@ -4,6 +4,8 @@ public class PitcherGame
 	{
 	static ArrayList<Pitch> pitch = new ArrayList<Pitch>();
 	static int[][] zone = new int[5][5];
+	static int row;
+	static int col;
 		public static void main(String[] args)
 			{
 			intro();
@@ -30,53 +32,113 @@ public class PitcherGame
 			System.out.println("What pitch would you like to throw \n\n \n(1) Four-Seam Fastball \n(2) Cutter \n(3) Splitter \n(4) Sinker \n(5) Curveball \n(6) Sweeper");
 			System.out.println("\nPlease input the number ex: 1");
 			int pitch = userInput.nextInt();
+			
+			if(pitch == 1)
+				{
+				fourSeam();
+				}
+			
+			else if(pitch ==2)
+				{
+				cutter();
+				}
+			
+			else if(pitch == 3)
+				{
+				splitter();
+				}
+			
+			else if(pitch == 4)
+				{
+				sinker();
+				}
+			
+			else if(pitch == 5)
+				{
+				curve();
+				}
+			
+			else if(pitch == 6)
+				{
+				sweeper();
+				}
+		
 			}
 		
-		public static int[] generateLocation(String pitchType)
+		public static int[] generateLocation()
 			{
 			int randomNumber = ((int)(Math.random()*1000));
-			
+			System.out.println(randomNumber);
 			int [] location = new int [2];
 			
-			for(int row = 0; row < 5; row++)
+			for(row = 0; row < 4; row++)
 				{
-				for(int col = 0; col < 5; col++)
+				for(col = 0; col < 4; col++)
 					{
-					zone[row][col];
+					if(zone[row][col] >= randomNumber)
+						{
+						System.out.println(zone[row][col]);
+						}
 					}
 				}
 			return location;
+			
 			}
 		
+		
+		public static void swingContactPecrent()
+			{
+			int percent = ((int)(Math.random()*100));
+			System.out.println(percent);
+			}
+			
 		public static void fourSeam()
 			{
 			System.out.println("Shohei Ohtani will throw a Four-Seam Fastball");
 			zone = Data.fourSeamPitch;
-			generateLocation(zone);
+			generateLocation();
+			fourSeamSwing();
 			}
 		
 		public static void cutter()
 			{
 			System.out.println("Shohei Ohtani will throw a Cutter");
+			zone = Data.cutterPitch;
+			generateLocation();
 			}
 		
 		public static void splitter()
 			{
 			System.out.println("Shohei Ohtani will throw a Splitter");
+			zone = Data.splitterPitch;
+			generateLocation();
 			}
 		
 		public static void sinker()
 			{
 			System.out.println("Shohei Ohtani will throw a Sinker");
+			zone = Data.sinkerPitch;
+			generateLocation();
 			}
 		
 		public static void curve()
 			{
 			System.out.println("Shohei Ohtani will throw a Curve");
+			zone = Data.curvePitch;
+			generateLocation();
 			}
 		
-		public static void slider()
+		public static void sweeper()
 			{
 			System.out.println("Shohei Ohtani will throw a Sweeper");
+			zone = Data.sweeperPitch;
+			generateLocation();
+			}
+		
+		public static void fourSeamSwing()
+			{
+			swingContactPecrent();
+			//zone = Data.fourSeamSwing;
+			System.out.println(Data.fourSeamSwing[row][col]);
 			}
 	}
